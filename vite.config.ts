d4b5dbae22ae4f811b42e694ca246d6b56b0761a
Aibek <aibek.abdykasymov@gmail.com>
2023-04-06 13:07:03 +0600

    Added svg import ability

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
   return {
-    plugins: [react()],
+    plugins: [react(), svgr({ exportAsDefault: true })],
     esbuild: {
       loader: "tsx",
       include: /src\/.*\.tsx?$/,
