2b6aa722d1cbabe969368c1658f59c5b41033f64
Aibek <aibek.abdykasymov@gmail.com>
2023-04-12 01:41:43 +0600

    Added new common types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,18 @@
 import { ReactNode } from "react";
 
 export type Children = string | JSX.Element | JSX.Element[] | ReactNode;
-
+export interface IUrl {
+  filename: string;
+  url: string;
+  expires: string;
+}
 export interface IImage {
   id: string;
-  url: string;
+  filename: string;
+  user: string;
+  url: IUrl;
+  thumbnail: IUrl;
   description: string;
-  //   createdAt: string;
-  //   updatedAt: string;
+  createdAt: string;
+  updatedAt: string;
 }
