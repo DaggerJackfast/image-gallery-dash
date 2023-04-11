22f0f9edaa88e745689b9cb48134154d2ce22832
Aibek <aibek.abdykasymov@gmail.com>
2023-04-12 01:50:45 +0600

    Changed header component, deleted unused links, added sidebar with showing profile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import cx from "classnames";
+import CloseSideIcon from "../../assets/icons/close-side-icon.svg";
+import Profile from "../Profile";
 
 const Header = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { logout, user, isAuthenticated } = useAuth0();
+  const [show, setShow] = useState<boolean>(false);
   return (
     <header className="bg-white">
       <nav
@@ -19,10 +23,12 @@ const Header = () => {
             />
           </a>
         </div>
-        <div className="flex lg:hidden">
+        <div className="flex">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 disabled:bg-slate-200"
+            onClick={() => setShow(true)}
+            disabled={show}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -41,32 +47,15 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Features
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Marketplace
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Company
-          </a>
-        </div>
-        {isAuthenticated && (
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <button
-              onClick={() => logout()}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Log out <span aria-hidden="true">&rarr;</span>
-            </button>
-          </div>
-        )}
       </nav>
 
-      <div className="lg:hidden" role="dialog" aria-modal="true">
+      <div
+        className={cx({ ["hidden"]: !show })}
+        role="dialog"
+        aria-modal="true"
+      >
         <div className="fixed inset-0 z-10"></div>
-        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 flex flex-col">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -79,125 +68,27 @@ const Header = () => {
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setShow(false)}
             >
               <span className="sr-only">Close menu</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <CloseSideIcon className="h-6 w-6" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="flex-1 flow-root">
+            {user && <Profile user={user} />}
+          </div>
+          <div className="mt-6 flex-0">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <div className="-mx-3">
+              <div className="space-y-2 py-6" />
+              <div className="py-6 flex justify-center">
+                {isAuthenticated && (
                   <button
-                    type="button"
-                    className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50"
-                    aria-controls="disclosure-1"
-                    aria-expanded="false"
+                    onClick={() => logout()}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Product
-                    <svg
-                      className="h-5 w-5 flex-none"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    Log Out <span aria-hidden="true">&rarr;</span>
                   </button>
-                  <div className="mt-2 space-y-2" id="disclosure-1">
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Analytics
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Engagement
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Security
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Integrations
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Automations
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Watch Watch demo
-                    </a>
-
-                    <a
-                      href="#"
-                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Contact Contact sales
-                    </a>
-                  </div>
-                </div>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log Log in
-                </a>
+                )}
               </div>
             </div>
           </div>
